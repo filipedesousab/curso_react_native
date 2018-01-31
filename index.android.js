a51fd9988c395dea7afccf8cf80519e6f56bd009
filipedesousab <filipegame07@gmail.com>
2018-01-31 11:40:10 -0300

    Gerador de números randomicos

--- index.android.js
+++ index.android.js
@@ -1,15 +1,25 @@
 var React = require('react');
 var Text = require('react-native').Text;
+var Button = require('react-native').Button;
+var View = require('react-native').View;
 var AppRegistry = require('react-native').AppRegistry;
 
-const App = function() {
+const geraNumeroAleatorio = () => {
+  var numero_aleatorio = Math.random();
+  numero_aleatorio = Math.floor(numero_aleatorio * 10);
+  alert(numero_aleatorio)
+}
+
+const App = () => {
   return (
-    <Text>
-      Meu Primeiro App
-    </Text>
+    <View>
+    <Text>Gerador de números randomicos</Text>
+    <Button
+      title="Gerar um número randônico"
+      onPress={geraNumeroAleatorio}
+    />
+    </View>
   )
 }
 
-AppRegistry.registerComponent('curso_react_native', function(){
-  return App
-})
+AppRegistry.registerComponent( 'curso_react_native', () => App )
