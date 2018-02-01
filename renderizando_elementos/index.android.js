f5e0ebcba241a703ab484ab930d8ee7377073d02
filipedesousab <filipegame07@gmail.com>
2018-02-01 02:06:41 -0300

    Alinhando Elementos com Flexbox

--- renderizando_elementos/index.android.js
+++ renderizando_elementos/index.android.js
@@ -10,21 +10,30 @@ const Estilos = {
     height: 60,
     width: 60
   },
+  estiloTexto2: {
+    fontSize: 40,
+    backgroundColor: '#2A48FA',
+    height: 60,
+    width: 60
+  },
   estiloView: {
     backgroundColor: 'skyblue',
     height: 300,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'column'
   }
 };
 // Criar componente
 const App = () => {
 
-  const { estiloTexto, estiloView } = Estilos;
+  const { estiloTexto, estiloTexto2, estiloView } = Estilos;
 
   return (
     <View style={ estiloView }>
       <Text style={ estiloTexto }>A</Text>
+      <Text style={ estiloTexto2 }>B</Text>
+      <Text style={ estiloTexto }>C</Text>
     </View>
   );
 };
