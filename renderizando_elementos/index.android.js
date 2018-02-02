c14aa74f1872c0a6d2b4bc2dbfd363e67a7b95b7
filipedesousab <filipegame07@gmail.com>
2018-02-01 21:02:05 -0300

    Adicionando imagens

--- renderizando_elementos/index.android.js
+++ renderizando_elementos/index.android.js
@@ -1,26 +1,17 @@
 // Import
 import React from 'react';
-import { Text, AppRegistry, View, TouchableOpacity } from 'react-native';
+import { Text, AppRegistry, View, Image } from 'react-native';
 
 // Formatações
 const Estilos = {
   principal: {
     paddingTop: 40
   },
-  botao: {
-    backgroundColor: '#4888EC',
-    padding: 10,
-    borderColor: '#1d8eb8',
-    borderWidth: 3,
-    borderRadius: 1
-  },
-  textoBotao: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    alignSelf: 'center'
+  imagem: {
+    justifyContent: 'flex-end',
+    padding: 5
   }
-};
+}
 
 const botaoPressionado = () => {
   alert('Botão pressionado')
@@ -28,17 +19,13 @@ const botaoPressionado = () => {
 // Criar componente
 const App = () => {
 
-  const { principal, botao, textoBotao } = Estilos;
+  const { principal, imagem } = Estilos;
 
   return (
     <View style={ principal }>
-      <TouchableOpacity
-        style={ botao }
-        onPress={ botaoPressionado }
-        accessibilityLabel="Clique para abrir as notícias!"
-      >
-        <Text style={ textoBotao }>Clique Aqui</Text>
-      </TouchableOpacity>
+      <Image style={ imagem } source={ require('./imgs/uvas.png') }>
+        <Text style={{backgroundColor: '#fff'}}>Legenda da foto</Text>
+      </Image>
     </View>
   );
 };
