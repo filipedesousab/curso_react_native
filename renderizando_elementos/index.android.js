ad7354e593ab580c195ae59636b705605703428a
filipedesousab <filipegame07@gmail.com>
2018-02-01 22:04:41 -0300

    App frases do dia - apresentando frases

--- renderizando_elementos/index.android.js
+++ renderizando_elementos/index.android.js
@@ -1,6 +1,6 @@
 // Import
 import React from 'react';
-import { Text, AppRegistry, View, Image, TouchableOpacity } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity, Alert } from 'react-native';
 
 // Formatações
 const Estilos = {
@@ -22,8 +22,19 @@ const Estilos = {
   }
 }
 
-const botaoPressionado = () => {
+const gerarNovaFrase = () => {
+  var numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor( numeroAleatorio * 5 );
 
+  var frases = Array();
+  frases[0] = 'Estou sempre alegre. Essa é a melhor maneira...';
+  frases[1] = 'O tempo dura bastante para aqueles que...';
+  frases[2] = 'Se você encontrar um caminho sem obstáculos...';
+  frases[3] = 'Não exite um caminho para a felicidade...';
+  frases[4] = 'Você nunca sabe a forca que tem. Até que...';
+
+  fraseEscolhida = frases[numeroAleatorio];
+  Alert.alert(fraseEscolhida)
 }
 // Criar componente
 const App = () => {
@@ -33,7 +44,10 @@ const App = () => {
   return (
     <View style={ principal }>
       <Image source={ require('./imgs/logo.png') } />
-      <TouchableOpacity style={ botao }>
+      <TouchableOpacity
+        onPress={ gerarNovaFrase }
+        style={ botao }
+      >
        <Text style={ textoBotao }>Nova Frase</Text>
       </TouchableOpacity>
     </View>
