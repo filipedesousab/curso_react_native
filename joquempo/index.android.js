de393dc69431c9a81e6cd6c1e84f374bd034a1d8
filipedesousab <filipegame07@gmail.com>
2018-02-07 21:21:37 -0300

    Codificando a lógica do jogo parte 2

--- joquempo/index.android.js
+++ joquempo/index.android.js
@@ -22,19 +22,56 @@ class joquempo extends Component {
   constructor(props){
     super(props);
 
-    this.state = { escolhaUsuario: ''}
+    this.state = { escolhaUsuario: '', escolhaComputador: '', resultado: ''}
   }
 
   joquempo( escolhaUsuario ){
-    this.setState({ escolhaUsuario: escolhaUsuario })
+    // Gerar número aleatório
+    var numAleatorio = Math.floor(Math.random() * 3);
+
+    var escolhaComputador = Array('pedra', 'papel', 'tesoura');
+
+    var resultado= '';
+
+    if(escolhaComputador[numAleatorio] == 'pedra'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Empate';
+      }else if(escolhaUsuario == 'papel'){
+        resultado = 'Usuário ganhou';
+      }else{
+        resultado = 'Computador ganhou';
+      }
+    }else if(escolhaComputador[numAleatorio] == 'papel'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Computador ganhou';
+      }else if(escolhaUsuario == 'papel'){
+        resultado = 'Empate';
+      }else{
+        resultado = 'Usuário ganhou';
+      }
+    }else{
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Usuário ganhou';
+      }else if(escolhaUsuario == 'papel'){
+        resultado = 'Computador ganhou';
+      }else{
+        resultado = 'Empate';
+      }
+    }
+
+    this.setState({
+      escolhaUsuario: escolhaUsuario,
+      escolhaComputador: escolhaComputador[numAleatorio],
+      resultado: resultado
+    })
   }
 
   render() {
     return (
       <View>
-        <Text>Escolha do computador</Text>
+        <Text>Escolha do computador: { this.state.escolhaComputador }</Text>
         <Text>Excolha do usuário: { this.state.escolhaUsuario }</Text>
-        <Text>Resultado</Text>
+        <Text>Resultado { this.state.resultado }</Text>
         <Button
           title="pedra"
           onPress={ () => { this.joquempo('pedra') } }
