1aa0f9dc3463a9846cbfef90703bf9c9e8b993b3
filipedesousab <filipegame07@gmail.com>
2018-02-14 17:32:34 -0300

    Efetuando requisições HTTP com axios

--- catalogo_de_produtos/src/components/listaItens.js
+++ catalogo_de_produtos/src/components/listaItens.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import {
   View
 } from 'react-native';
+import axios from 'axios';
 
 import Itens from './itens';
 
 export default class listaItens extends Component {
+
+  componentWillMount() {
+    // Requisiçã HTTP
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+      .then((response) => { console.log(response.data); })
+      .catch((err) => { console.log('Erro ao recuperar os dados'); });
+  }
+
   render() {
     return (
       <View>
