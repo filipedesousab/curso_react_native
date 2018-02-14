3b405609dbfd7a7090fc07c0b3b4973a78d7ce85
filipedesousab <filipegame07@gmail.com>
2018-02-14 16:27:34 -0300

    Criando o componente Itens

--- catalogo_de_produtos/src/components/listaItens.js
+++ catalogo_de_produtos/src/components/listaItens.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
 import {
-  Text
+  View
 } from 'react-native';
 
-export default class catalogo_de_produtos extends Component {
-  constructor(props) {
-    super(props);
-    console.log('Construindo a aplicação');
-  }
+import Itens from './itens';
 
-  componentWillMount() {
-    console.log('Fazer alguma coisa antes de renderizar');
-  }
-
-  componentDidMount() {
-    console.log('Fazer alguma coisa depois da renderização');
-    console.log(console);
-  }
-
+export default class listaItens extends Component {
   render() {
-    console.log('Objeto é renderizado');
     return (
-      <Text>Testando importação do componente</Text>
+      <View>
+        <Itens />
+        <Itens />
+        <Itens />
+      </View>
     );
   }
 }
