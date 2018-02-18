0fff3d0c528ef223654f60b8dcd062925bf68c73
filipedesousab <filipegame07@gmail.com>
2018-02-17 23:11:58 -0300

    Passando os metodos atualizarValor, atualizarOperacao e os states num1, num2 e operacao para o App A partir dai todos os componentes serão controlados pelo componente pai App changes. Lines starting

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -8,9 +8,11 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { resultado: '' };
+    this.state = { num1: '12', num2: '12', operacao: 'soma', resultado: '' };
 
     this.calcular = this.calcular.bind(this);
+		this.atualizaValor = this.atualizaValor.bind(this);
+		this.atualizaOperacao = this.atualizaOperacao.bind(this);
   }
 
   calcular(num1, num2, operacao) {
@@ -26,12 +28,30 @@ export default class App extends Component {
     this.setState({ resultado: String(resultado) });;
   }
 
+  atualizaOperacao(operacao) {
+		this.setState({ operacao });
+	}
+
+	atualizaValor(nomeCampo, valor) {
+		const obj = {};
+		obj[nomeCampo] = valor;
+
+		this.setState(obj);
+	}
+
   render() {
     return (
       <View>
         <Topo />
         <Resultado resultado={this.state.resultado} />
-        <Painel calcular={this.calcular}/>
+        <Painel
+          num1={this.state.num1}
+          num2={this.state.num2}
+          operacao={this.state.operacao}
+          calcular={this.calcular}
+          atualizaValor={this.atualizaValor}
+          atualizaOperacao={this.atualizaOperacao}
+        />
       </View>
     )
   }
--- calculadora/src/components/Painel.js
+++ calculadora/src/components/Painel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
 	Text,
 	View
@@ -8,46 +8,24 @@ import Entrada from './Entrada';
 import Operacao from './Operacao';
 import Comando from './Comando';
 
-class Painel extends Component{
-
-	constructor(props) {
-		super(props);
-
-		this.state = { num1: '12', num2: '12', operacao: 'soma' };
-
-		this.atualizaValor = this.atualizaValor.bind(this);
-		this.atualizaOperacao = this.atualizaOperacao.bind(this);
-	}
-
-	atualizaOperacao(operacao) {
-		this.setState({ operacao });
-	}
-
-	atualizaValor(nomeCampo, valor) {
-		const obj = {};
-		obj[nomeCampo] = valor;
-
-		this.setState(obj);
-	}
-
-	render() {
-		return (
-		  <View>
-		  	<Entrada
-					num1={this.state.num1}
-					num2={this.state.num2}
-					atualizaValor={this.atualizaValor}
-				/>
-				<Operacao operacao={this.state.operacao} acao={this.atualizaOperacao} />
-				<Comando
-					acao={this.props.calcular}
-					num1={this.state.num1}
-					num2={this.state.num2}
-					operacao={this.state.operacao}
-				/>
-		  </View>
-		);
-	}
-}
+const Painel = props => (
+	<View>
+		<Entrada
+			num1={props.num1}
+			num2={props.num2}
+			atualizaValor={props.atualizaValor}
+		/>
+		<Operacao
+			operacao={props.operacao}
+			acao={props.atualizaOperacao}
+		/>
+		<Comando
+			acao={props.calcular}
+			num1={props.num1}
+			num2={props.num2}
+			operacao={props.operacao}
+		/>
+	</View>
+	);
 
 export { Painel };
