28928821a5fb38226df2a67dadc610613304358f
filipedesousab <filipegame07@gmail.com>
2018-02-20 12:32:20 -0300

    Entendendo como salva e recupera dados do firebase

--- firebaseTeste/index.android.js
+++ firebaseTeste/index.android.js
@@ -1,53 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
+  Button,
   Text,
   View
 } from 'react-native';
+import firebase from 'firebase';
 
 export default class firebaseTeste extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { pontuacao: 0 };
+  }
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyB_0Pa8QhqYChncABCi4NZcLbSYQJ4Y1bQ",
+      authDomain: "configuracaofirebasereact2107.firebaseapp.com",
+      databaseURL: "https://configuracaofirebasereact2107.firebaseio.com",
+      projectId: "configuracaofirebasereact2107",
+      storageBucket: "configuracaofirebasereact2107.appspot.com",
+      messagingSenderId: "953986329042"
+    };
+
+    firebase.initializeApp(config);
+  }
+
+  salvarDados() {
+    let funcionarios = firebase.database().ref('funcionarios');
+    // database.ref('pontuacao').remove();
+
+    funcionarios.push().child('nome').set(
+      {
+        nome: 'Mariana SIlva',
+        altura: '1,65',
+        peso: '60KG'
+      }
+    );
+    // funcionarios.remove();
+
+  }
+
+  listarDados() {
+    let pontuacao = firebase.database().ref('pontuacao');
+
+    pontuacao.on('value', snapshot => {
+      let pontos = snapshot.val();
+      this.setState({ pontuacao: pontos });
+    });
+  }
+
   render() {
+
+    let {pontuacao} = this.state;
+    
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+      <View>
+        <Button
+          title="Salvar Dados"
+          onPress={ () => { this.salvarDados() } }
+        />
+        <Button
+          title="Listar Dados"
+          onPress={ () => { this.listarDados() } }
+        />
+        <Text>
+          {pontuacao}
         </Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('firebaseTeste', () => firebaseTeste);
