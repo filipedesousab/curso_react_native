272ec76d673c80506b14ce45bc788131987fd31f
filipedesousab <filipegame07@gmail.com>
2018-02-22 20:16:51 -0300

    Adicionando imagem de background aos componentes FormLogin e FormCadastro

--- whatsappClone/src/components/formCadastro.js
+++ whatsappClone/src/components/formCadastro.js
@@ -1,46 +1,48 @@
 import React from 'react';
-import { Button, View, TextInput } from 'react-native';
+import { Button, Image, View, TextInput } from 'react-native';
 import { connect } from 'react-redux';
 
 import { modificaEmail, modificaSenha, modificaNome } from '../actions/AutenticacaoAction';
 
 const formCadastro = props => (
-  <View style={{ flex: 1, padding: 10 }}>
+  <Image style={{ flex: 1, width: null }} source={require('../imgs/bg.png')}>
+    <View style={{ flex: 1, padding: 10 }}>
 
-    <View style={{ flex: 4, justifyContent: 'center' }}>
-      <TextInput
-        style={{ fontSize: 20, height: 45 }}
-        placeholder='Nome'
-        editable={true}
-        value={props.nome}
-        onChangeText={ texto => props.modificaNome(texto) }
-      />
-      <TextInput
-        style={{ fontSize: 20, height: 45 }}
-        placeholder='E-mail'
-        editable={true}
-        value={props.email}
-        onChangeText={ texto => props.modificaEmail(texto) }
-      />
-      <TextInput
-        style={{ fontSize: 20, height: 45 }}
-        placeholder='Senha'
-        editable={true}
-        secureTextEntry={true}
-        value={props.senha}
-        onChangeText={ texto => props.modificaSenha(texto) }
-      />
+      <View style={{ flex: 4, justifyContent: 'center' }}>
+        <TextInput
+          style={{ fontSize: 20, height: 45 }}
+          placeholder='Nome'
+          editable={true}
+          value={props.nome}
+          onChangeText={ texto => props.modificaNome(texto) }
+        />
+        <TextInput
+          style={{ fontSize: 20, height: 45 }}
+          placeholder='E-mail'
+          editable={true}
+          value={props.email}
+          onChangeText={ texto => props.modificaEmail(texto) }
+        />
+        <TextInput
+          style={{ fontSize: 20, height: 45 }}
+          placeholder='Senha'
+          editable={true}
+          secureTextEntry={true}
+          value={props.senha}
+          onChangeText={ texto => props.modificaSenha(texto) }
+        />
+      </View>
+
+      <View style={{ flex: 1 }}>
+        <Button
+          color='#115E54'
+          title='Cadastrar'
+          onPress={ () => {  } }
+        />
+      </View>
+
     </View>
-
-    <View style={{ flex: 1 }}>
-      <Button
-        color='#115E54'
-        title='Cadastrar'
-        onPress={ () => {  } }
-      />
-    </View>
-
-  </View>
+  </Image>
 );
 
 const mapStateToProps = state => (
--- whatsappClone/src/components/formLogin.js
+++ whatsappClone/src/components/formLogin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import { Button, Image, View, Text, TextInput, TouchableHighlight } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 
@@ -7,46 +7,48 @@ import { modificaEmail, modificaSenha } from '../actions/AutenticacaoAction';
 
 const formLogin = props => {
   return (
-    <View style={{ flex: 1, padding: 10 }}>
+    <Image style={{ flex: 1, width: null }} source={require('../imgs/bg.png')}>
+      <View style={{ flex: 1, padding: 10 }}>
 
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 25 }}>Whatsapp Clone</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ fontSize: 25 }}>Whatsapp Clone</Text>
+        </View>
+
+        <View style={{ flex: 2 }}>
+          <TextInput
+            style={{ fontSize: 20, height: 45 }}
+            placeholder='E-mail'
+            editable={true}
+            value={props.email}
+            onChangeText={ texto => props.modificaEmail(texto) }
+          />
+          <TextInput
+            style={{ fontSize: 20, height: 45 }}
+            placeholder='Senha'
+            editable={true}
+            secureTextEntry={true}
+            value={props.senha}
+            onChangeText={ texto => props.modificaSenha(texto) }
+          />
+          <TouchableHighlight
+            onPress={() => {
+              Actions.cadastro();
+            }}
+          >
+            <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <Button
+            color='#115E54'
+            title='Acessar'
+            onPress={ () => {  } }
+          />
+        </View>
+
       </View>
-
-      <View style={{ flex: 2 }}>
-        <TextInput
-          style={{ fontSize: 20, height: 45 }}
-          placeholder='E-mail'
-          editable={true}
-          value={props.email}
-          onChangeText={ texto => props.modificaEmail(texto) }
-        />
-        <TextInput
-          style={{ fontSize: 20, height: 45 }}
-          placeholder='Senha'
-          editable={true}
-          secureTextEntry={true}
-          value={props.senha}
-          onChangeText={ texto => props.modificaSenha(texto) }
-        />
-        <TouchableHighlight
-          onPress={() => {
-            Actions.cadastro();
-          }}
-        >
-          <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
-        </TouchableHighlight>
-      </View>
-
-      <View style={{ flex: 1 }}>
-        <Button
-          color='#115E54'
-          title='Acessar'
-          onPress={ () => {  } }
-        />
-      </View>
-
-    </View>
+    </Image>
   );
 }
 
