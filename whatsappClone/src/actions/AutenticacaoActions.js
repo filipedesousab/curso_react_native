81025049c787b182dc747ae15ed5d36bcd5ed5a4
filipedesousab <filipegame07@gmail.com>
2018-02-23 13:47:08 -0300

    Configurando o redux-thunk para trabalhar no cadastro de usuario

--- whatsappClone/src/actions/AutenticacaoActions.js
+++ whatsappClone/src/actions/AutenticacaoActions.js
@@ -28,13 +28,32 @@ export const modificaNome = (texto) => {
 }
 
 export const cadastraUsuario = ({ nome, email, senha }) => {
-
-  firebase.auth().createUserWithEmailAndPassword(email, senha)
-    .then(user => console.log(user) )
-    .catch(erro => console.log(erro));
-
-  return {
-    type: 'cadastra_usuario',
-    payload: ''
-  };
+  return dispatch => {
+    firebase.auth().createUserWithEmailAndPassword(email, senha)
+      .then(user => cadastroUsuarioSucesso(dispatch) )
+      .catch(erro => cadastraUsuarioErro(erro, dispatch));
+  }
 };
+
+const cadastroUsuarioSucesso = (dispatch) => {
+  dispatch ( { type: 'sucesso' } );
+}
+
+const cadastraUsuarioErro = (erro, dispatch) => {
+  dispatch ( { type: 'erro' } );
+  // The email address is badly formatted.
+  // O endereço de e-mail está mal formatado.
+  // auth/invalid-email
+
+  // The email address is already in use by another account.
+  // O endereço de e-mail já está sendo usado por outra conta.
+  // auth/email-already-in-use
+
+  // The password must be 6 characters long or more.
+  // A senha deve ter 6 caracteres ou mais.
+  // auth/weak-password
+
+  // Password should be at least 6 characters
+  // A senha deve ter pelo menos 6 caracteres
+  // auth/weak-password
+}
