8bfd9c455b76b9ea4b812ae4e429bdbfa0b029e9
filipedesousab <filipegame07@gmail.com>
2018-02-23 14:30:14 -0300

    Criando o componente boasVindas

--- whatsappClone/src/Routes.js
+++ whatsappClone/src/Routes.js
@@ -5,10 +5,12 @@ import { Router, Scene } from 'react-native-router-flux';
 
 import FormLogin from './components/formLogin';
 import FormCadastro from './components/formCadastro';
+import BoasVindas from './components/boasVindas';
 
 export default props => (
   <Router sceneStyle={{ paddingTop: 50 }}>
     <Scene key='login' component={FormLogin} title="Login" />
     <Scene key='cadastro' component={FormCadastro} title="Cadastro" />
+    <Scene key='boasVindas' component={BoasVindas} title="Bem Vindo" initial/>
   </Router>
 );
