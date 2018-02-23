52c06f36e61173462c130ae9704fb7f74085de96
filipedesousab <filipegame07@gmail.com>
2018-02-22 22:10:48 -0300

    Adicionando Firebase ao projeto

--- whatsappClone/src/App.js
+++ whatsappClone/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers';
 
-export default props => (
-  <Provider store={createStore(reducers)}>
-    <Routes />
-  </Provider>
-);
+class App extends Component {
+
+  componentWillMount() {
+    // Initialize Firebase
+    let config = {
+      apiKey: "AIzaSyDattViIm-n-ClANZWtY786OBCJa7wvSIk",
+      authDomain: "whatsapp-clone-2107.firebaseapp.com",
+      databaseURL: "https://whatsapp-clone-2107.firebaseio.com",
+      projectId: "whatsapp-clone-2107",
+      storageBucket: "whatsapp-clone-2107.appspot.com",
+      messagingSenderId: "277972349141"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <Routes />
+      </Provider>
+    );
+  }
+}
+
+export default App;
