1b03a2d0a85735e1be1df26e14e5d4524a77eed7
filipedesousab <filipegame07@gmail.com>
2018-02-24 16:57:24 -0300

    Substituino fi else por switch

--- whatsappClone/src/reducers/AutenticacaoReducer.js
+++ whatsappClone/src/reducers/AutenticacaoReducer.js
@@ -10,19 +10,20 @@ export default (state = INITIAL_STATE, action) => {
   console.log('AutenticacaoReducer');
   console.log(action);
 
-  if (action.type === 'modifica_email') {
-    return { ...state, email: action.payload };
-  } else if (action.type === 'modifica_senha') {
-    return { ...state, senha: action.payload };
-  } else if (action.type === 'modifica_nome') {
-    return { ...state, nome: action.payload };
-  } else if (action.type === 'cadastro_usuario_erro') {
-    return { ...state, erroCadastro: action.payload };
-  } else if (action.type === 'cadastro_usuario_sucesso') {
-    return { ...state, nome: '', senha: '' };
-  } else if (action.type === 'login_usuario_erro') {
-    return { ...state, erroLogin: action.payload };
+  switch (action.type) {
+    case 'modifica_email':
+      return { ...state, email: action.payload };
+    case 'modifica_senha':
+      return { ...state, senha: action.payload };
+    case 'modifica_nome':
+      return { ...state, nome: action.payload };
+    case 'cadastro_usuario_erro':
+      return { ...state, erroCadastro: action.payload };
+    case 'cadastro_usuario_sucesso':
+      return { ...state, nome: '', senha: '' };
+    case 'login_usuario_erro':
+      return { ...state, erroLogin: action.payload };
+    default:
+      return state;
   }
-
-  return state;
 }
